5d97a1517d7a15cdcc7fe99139a0ab76d0ed780c
Olexandr Hoshylyk <gashuk95@gmail.com>
2019-01-01 09:41:39 +0000

    Fix bug with server rendering

--- src/CustomRouteSwitch/validators/pathValidator.js
+++ src/CustomRouteSwitch/validators/pathValidator.js
@@ -1,9 +1,9 @@
 import { matchPath, Redirect } from 'react-router-dom';
 
 const pathValidator = (child, props) => {
-  const { match } = props,
-    { exact, strict, sensitive } = child.props,
-    childPath = child.type === Redirect ? child.props.from : child.props.path;
+  const { match, location } = props;
+  const { exact, strict, sensitive } = child.props;
+  const childPath = child.type === Redirect ? child.props.from : child.props.path;
   return !!matchPath(
     location.pathname,
     { path: childPath, exact, strict, sensitive },
