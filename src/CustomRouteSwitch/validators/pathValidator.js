be13e9d1e180709db8ad6789e4018a9e4dad5fc3
Olexandr Hoshylyk <gashuk95@gmail.com>
2021-05-13 08:58:13 +0300

    FIX typo

--- src/CustomRouteSwitch/validators/pathValidator.js
+++ src/CustomRouteSwitch/validators/pathValidator.js
@@ -3,7 +3,7 @@ import { useRouteMatch, Redirect } from 'react-router-dom';
 const pathValidator = (child, props) => {
   const { exact, strict, sensitive } = child.props;
   const childPath = (child.type === Redirect ? child.props.from : child.props.path) || '';
-  return !!useRouteMatch(path: childPath, exact, strict, sensitive);
+  return !!useRouteMatch({ path: childPath, exact, strict, sensitive });
 };
 
 export default pathValidator;
